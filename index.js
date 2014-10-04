4cda46b7d0bd07dd7f6e93dbf33635ac89339fdd
Chris Santero <chris@solveitsimply.com>
2014-10-04 19:47:07 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var path = require('path');
+var symlinkOrCopySync = require('symlink-or-copy').sync;
+
+module.exports = PeekPlugin;
+
+function PeekPlugin (inputTree, peekHandler, cleanupHandler) {
+  if (!(this instanceof PeekPlugin)) {
+    return new PeekPlugin(inputTree, peekHandler, cleanupHandler);
+  }
+  this.inputTree = inputTree;
+  this.peekHandler = peekHandler;
+  this.cleanupHandler = cleanupHandler;
+}
+
+PeekPlugin.prototype.read = function (readTree) {
+  var self = this;
+  return readTree(this.inputTree).then(function (srcDir) {
+    self.destDirName = './tmp/peek_at-' + path.basename(srcDir);
+    symlinkOrCopySync(srcDir, self.destDirName);
+    if (self.peekHandler) {
+      self.peekContext = self.peekHandler(srcDir);
+    }
+    return self.destDirName;
+  });
+};
+
+PeekPlugin.prototype.cleanup = function () {
+  if (this.cleanupHandler) {
+    this.cleanupHandler(this.destDirName, this.peekContext);
+  }
+  fs.unlinkSync(this.destDirName);
+};
